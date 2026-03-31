bbfbabfe37785bf352dd749c4157f5ff34790bbe
garysinghtrading-dot <gary.singh.trading@gmail.com>
2026-03-31 07:13:10 +0000

    changed port settings

--- app.py
+++ app.py
@@ -167,4 +167,5 @@ def options_trade():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=False)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port, debug=False)
